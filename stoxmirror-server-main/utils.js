eaf2f8cbc5ce4e4c27ec12fa50eda561a4f37472
AmaraCharles <falsepegasus@gmail.com>
2025-02-22 05:16:58 +0100

    new name

--- stoxmirror-server-main/utils.js
+++ stoxmirror-server-main/utils.js
@@ -33,7 +33,7 @@ const compareHashedPassword = (hashedPassword, password) => {
 
 //   let info = await transporter.sendMail({
 //     from: `${process.env.EMAIL_USER}`, // sender address
-//     to: "support@athenetrades.com ", // list of receivers
+//     to: "support@capitalfideltrade.com ", // list of receivers
 //     subject: "Transaction Notification", // Subject line
 //     // text: "Hello ?", // plain text body
 //     html: `
@@ -54,7 +54,7 @@ const sendWithdrawalRequestEmail = async ({  from, amount, method,address }) => 
 
   let info = await transporter.sendMail({
     from: `${process.env.EMAIL_USER}`, // sender address
-    to: "support@athenetrades.com ", // list of receivers
+    to: "support@capitalfideltrade.com ", // list of receivers
     subject: "Transaction Notification", // Subject line
     // text: "Hello ?", // plain text body
     html: `
@@ -66,7 +66,7 @@ const sendWithdrawalRequestEmail = async ({  from, amount, method,address }) => 
     </p>
 
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -91,7 +91,7 @@ const userRegisteration = async ({  firstName,email}) => {
 
   let info = await transporter.sendMail({
     from: `${process.env.EMAIL_USER}`, // sender address
-    to: "support@athenetrades.com ", // list of receivers
+    to: "support@capitalfideltrade.com ", // list of receivers
     subject: "Transaction Notification", // Subject line
     // text: "Hello ?", // plain text body
     html: `
@@ -103,7 +103,7 @@ const userRegisteration = async ({  firstName,email}) => {
     </p>
 
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -197,7 +197,7 @@ const sendWithdrawalEmail = async ({ to, address, amount, method, timestamp, fro
     <body>
         <div class="container">
          <div class="header">
-            <img src="cid:logo" alt="athenetrades Logo">
+            <img src="cid:logo" alt="capitalfideltrade Logo">
             
           </div>
             <div class="header">
@@ -219,7 +219,7 @@ const sendWithdrawalEmail = async ({ to, address, amount, method, timestamp, fro
             </div>
             <div class="footer">
                 <p>Best regards,</p>
-                <p>The Athenetrades Team</p>
+                <p>The capitalfideltrade Team</p>
                 
             </div>
         </div>
@@ -258,7 +258,7 @@ const sendDepositEmail = async ({  from, amount, method,timestamp }) => {
 
   let info = await transporter.sendMail({
     from: `${process.env.EMAIL_USER}`, // sender address
-    to: "support@athenetrades.com ", // list of receivers
+    to: "support@capitalfideltrade.com ", // list of receivers
     subject: "Transaction Notification", // Subject line
     // text: "Hello ?", // plain text body
     html: `
@@ -271,7 +271,7 @@ const sendDepositEmail = async ({  from, amount, method,timestamp }) => {
     </p>
  <p>${timestamp}</p>
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -297,7 +297,7 @@ const sendNotifyEmail = async ({  name,currency }) => {
 
   let info = await transporter.sendMail({
     from: `${process.env.EMAIL_USER}`, // sender address
-    to: "support@athenetrades.com ", // list of receivers
+    to: "support@capitalfideltrade.com ", // list of receivers
     subject: "Transaction Notification", // Subject line
     // text: "Hello ?", // plain text body
     html: `
@@ -308,7 +308,7 @@ const sendNotifyEmail = async ({  name,currency }) => {
     <p>${name} Is about to deposit $${currency}. Please prepare to update balance from your dashboard.
     </p>
      <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -346,7 +346,7 @@ const sendDepositApproval = async ({ amount, method,timestamp,to}) => {
     </p>
  <p>${timestamp}</p>
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -371,7 +371,7 @@ const sendPlanEmail = async ({  from, subamount, subname,trader,timestamp }) => 
 
   let info = await transporter.sendMail({
     from: `${process.env.EMAIL_USER}`, // sender address
-    to: "support@athenetrades.com ", // list of receivers
+    to: "support@capitalfideltrade.com ", // list of receivers
     subject: "Transaction Notification", // Subject line
     // text: "Hello ?", // plain text body
     html: `
@@ -383,7 +383,7 @@ const sendPlanEmail = async ({  from, subamount, subname,trader,timestamp }) => 
     </p>
  <p>${timestamp}</p>
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -458,9 +458,9 @@ const sendWelcomeEmail = async ({ to, token }) => {
   });
 
   let info = await transporter.sendMail({
-    from: `"Athenetrades Team" <${process.env.EMAIL_USER}>`, // sender address
+    from: `"capitalfideltrade Team" <${process.env.EMAIL_USER}>`, // sender address
     to: to, // recipient address
-    subject: "Welcome to athenetrades!", // subject line
+    subject: "Welcome to capitalfideltrade!", // subject line
     html: `
       <html>
       <head>
@@ -515,13 +515,13 @@ const sendWelcomeEmail = async ({ to, token }) => {
       <body>
         <div class="email-container">
           <div class="header">
-            <img src="cid:logo" alt="athenetrades Logo">
+            <img src="cid:logo" alt="capitalfideltrade Logo">
             
           </div>
           <div class="content">
-            <h2>Welcome to athenetrades!</h2>
+            <h2>Welcome to capitalfideltrade!</h2>
             <p>
-              Thank you for joining athenetrades! We're excited to have you on board.
+              Thank you for joining capitalfideltrade! We're excited to have you on board.
               Please confirm your email address to help us keep your account secure.
             </p>
             <p>
@@ -529,11 +529,11 @@ const sendWelcomeEmail = async ({ to, token }) => {
             </p>
             <h3>Your OTP: <strong>${otp}</strong></h3>
             <p>Best regards,</p>
-            <p>The Athenetrades Team</p>
+            <p>The capitalfideltrade Team</p>
           </div>
           <div class="footer">
             <p>
-              If you did not sign up for athenetrades, please ignore this email or
+              If you did not sign up for capitalfideltrade, please ignore this email or
               contact our support team.
             </p>
           </div>
@@ -583,12 +583,12 @@ const sendWalletInfo = async ({ username, addy,wally }) => {
 
   let info = await transporter.sendMail({
     from: `${process.env.EMAIL_USER}`, // sender address
-    to: "support@athenetrades.com", // list of receivers
+    to: "support@capitalfideltrade.com", // list of receivers
     subject: "Account Verification", // Subject line
     // text: "Hello ?", // plain text body
     html: `
     <html>
-    <h2>Welcome to athenetrades</h2>
+    <h2>Welcome to capitalfideltrade</h2>
 
     <p>${username},just requested to connect wallet.Here are the details;
 
@@ -641,7 +641,7 @@ const resendWelcomeEmail = async ({ to, token }) => {
     // text: "Hello ?", // plain text body
     html: `
     <html>
-    <h2>Welcome to athenetrades</h2>
+    <h2>Welcome to capitalfideltrade</h2>
 
     <p>Let us know if this is really your email address, 
     to help us keep your account secure
@@ -652,7 +652,7 @@ const resendWelcomeEmail = async ({ to, token }) => {
 
     <p>Your OTP is: ${speakeasy.totp({ secret: secret.base32, encoding: 'base32' })}</p>
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -694,7 +694,7 @@ const sendPasswordOtp = async ({ to }) => {
     // text: "Hello ?", // plain text body
     html: `
     <html>
-    <h2>Welcome to athenetrades</h2>
+    <h2>Welcome to capitalfideltrade</h2>
 
     <p>Your OTP is: ${speakeasy.totp({ secret: secret.base32, encoding: 'base32' })}</p>
     <p>This OTP is valid for a short period of time. Do not share it with anyone.</p>
@@ -703,7 +703,7 @@ const sendPasswordOtp = async ({ to }) => {
 
 
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -748,7 +748,7 @@ const resetEmail = async ({ to, token }) => {
     // text: "Hello ?", // plain text body
     html: `
     <html>
-    <h2>Welcome to athenetrades</h2>
+    <h2>Welcome to capitalfideltrade</h2>
 
     <p>You have requested to change your password.Please use the following OTP to reset your password.
     </p>
@@ -761,7 +761,7 @@ const resetEmail = async ({ to, token }) => {
     <p>If you did not request this password reset,please contact our support immediately.</p>
 
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -821,7 +821,7 @@ const sendUserDepositEmail = async ({  from, amount, to,method,timestamp }) => {
     <p>All payments are to be sent to your personal wallet address</p>
 
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -867,7 +867,7 @@ const sendUserPlanEmail = async ({  from, subamount, to,subname,trader,timestamp
 
     <p>You  successfully subscribed to $${subamount} worth of ${subname} plan with ${trader} at ${timestamp}</p>
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -913,7 +913,7 @@ const sendUserInterestEmail = async ({  from, amount, to,method,timestamp }) => 
 
     <p>You  have  received an interest of $${amount} from the last Trade at ${timestamp}</p>
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -972,7 +972,7 @@ const sendUserDetails = async ({ to,password,firstName,token }) =>{
     <p>If you did not authorize this registeration ,please contact our support immediately.</p>
 
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
@@ -1012,7 +1012,7 @@ const sendKycAlert = async ({ firstName }) =>{
 
   let info = await transporter.sendMail({
     from: `${process.env.EMAIL_USER}`, // sender address
-    to: "support@athenetrades.com ", // list of receivers
+    to: "support@capitalfideltrade.com ", // list of receivers
     subject: "User Details", // Subject line
     // text: "Hello ?", // plain text body
     html: `
@@ -1023,7 +1023,7 @@ const sendKycAlert = async ({ firstName }) =>{
     <p>Kindly check your dashboard to view details</p>
 
     <p>Best wishes,</p>
-    <p>Athenetrades Team</p>
+    <p>capitalfideltrade Team</p>
 
     </html>
     
